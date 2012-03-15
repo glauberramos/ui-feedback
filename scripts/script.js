60b2bbc9c3f22ce8a6ae5725107d06c042e06b2a
Glauber Ramos <glauberamos@gmail.com>
2012-03-15 17:01:39 -0300

    adding data.js for initial data

--- scripts/script.js
+++ scripts/script.js
@@ -3,24 +3,6 @@ var commentsTemplate = '<div class="feedback"><div class="feedback-area"/><label
 
 var showOrHide = true;
 
-var initialData = [{
-  id: 1,
-  description: 'need to improve button',
-  top: 200,
-  left: 200,
-  user_name: 'gramos',
-  progress: 'open',
-  date: '03/10/2012'
-},{
-  id: 2,
-  description: 'need to improve layout design',
-  top: 350,
-  left: 300,
-  user_name: 'gramos',
-  progress: 'open',
-  date: '03/10/2012'
-}];
-
 function createFeedback(feedbackData) {
   return $(Mustache.render(commentsTemplate, feedbackData));
 };
