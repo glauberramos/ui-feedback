1c444fa4157099064b36399ed6b6f7ab72c7fc7c
Glauber Ramos <glauberamos@gmail.com>
2012-03-15 17:05:53 -0300

    missing file

---
+++ scripts/data.js
@@ -0,0 +1,17 @@
+var initialData = [{
+  id: 1,
+  description: 'need to improve button',
+  top: 200,
+  left: 200,
+  user_name: 'gramos',
+  progress: 'open',
+  date: '03/10/2012'
+},{
+  id: 2,
+  description: 'need to improve layout design',
+  top: 350,
+  left: 300,
+  user_name: 'gramos',
+  progress: 'open',
+  date: '03/10/2012'
+}];
