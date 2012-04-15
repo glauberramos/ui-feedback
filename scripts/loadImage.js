6f1f9ebb6933d307bf051d84220936f0642bc35c
Gramos <gramos@tech-opss-MacBook-Pro-9.local>
2012-04-15 00:29:59 -0700

    changing facebook creation to loadImage file

--- scripts/loadImage.js
+++ scripts/loadImage.js
@@ -1,6 +1,22 @@
 var userImage;
 
 $(function(){
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '119092938223182',
+    status     : true,
+    cookie     : true,
+    xfbml      : true,
+    oauth      : true,
+  });
+  };
+  (function(d){
+    var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    d.getElementsByTagName('head')[0].appendChild(js);
+  }(document));
+
   FB.api({
     method: 'fql.query',
     query: 'SELECT pic_square FROM user WHERE uid=me()'
